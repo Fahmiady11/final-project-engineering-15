3731314d872db0ea5ac08755669cd63cf3ac65bc
Nicolas Sanjaya <nicolas.sanjaya701@gmail.com>
2022-06-18 17:18:43 +0700

    FPC-16 UpdateAboutPage

--- src/Component/pages/AboutPage.js
+++ src/Component/pages/AboutPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import imgAbout from '../../Assets/img-about.svg';
-import avatar from '../../Assets/avatar-about.svg';
 import github from '../../Assets/github.svg';
 import linkedin from '../../Assets/linkedIn.svg';
 import Navbar from '../molecules/Navbar';
 import Footer from '../molecules/Footer';
+import fahmi from '../../Assets/About/fahmi.jpg';
+import rachmat from '../../Assets/About/rachmat.jpg';
+import nico from '../../Assets/About/nico.jpg';
+import kevin from '../../Assets/About/kevin.jpg';
+import winnie from '../../Assets/About/winnie.jpg';
+import yusuf from '../../Assets/About/yusuf.jpg';
 
 
 function AboutPage() {
@@ -12,44 +17,56 @@ function AboutPage() {
     {
       name: 'Muhammad Fahmi Ady Susilo',
       role: 'Frontend Developer',
-      image: avatar,
+      image: fahmi,
       github: github,
       linkedin: linkedin,
+      linkGithub: 'https://github.com/Fahmiady11/',
+      linkLinkedin: 'https://www.linkedin.com/in/muhammad-fahmi-ady-susilo-ba05a41b0/',
     },
     {
       name: 'Rachmat Agung Ananda',
       role: 'Backend Developer',
-      image: avatar,
+      image: rachmat,
       github: github,
       linkedin: linkedin,
+      linkGithub: 'https://github.com/rchmatagung',
+      linkLinkedin: 'https://www.linkedin.com/in/rachmat-agung-ananda-013725173/',
     },
     {
       name: 'Nicolas Sanjaya',
       role: 'Frontend Developer',
-      image: avatar,
+      image: nico,
       github: github,
       linkedin: linkedin,
+      linkGithub: 'https://github.com/nicolsan',
+      linkLinkedin: 'https://www.linkedin.com/in/nicolas-sanjaya/',
     },
     {
       name: 'Kevin Maulana Nasrullah',
       role: 'Backend Developer',
-      image: avatar,
+      image: kevin,
       github: github,
       linkedin: linkedin,
+      linkGithub: 'https://github.com/kevinmaulanan',
+      linkLinkedin: 'https://www.linkedin.com/in/kevin-maulana-nasrullah/',
     },
     {
       name: 'Winnie Monica',
       role: 'Backend Developer',
-      image: avatar,
+      image: winnie,
       github: github,
       linkedin: linkedin,
+      linkGithub: 'https://github.com/winniemonica',
+      linkLinkedin: 'https://www.linkedin.com/in/winnie-monica-87b280207/',
     },
     {
       name: 'Yusuf Supriadi',
       role: 'Backend Developer',
-      image: avatar,
+      image: yusuf,
       github: github,
       linkedin: linkedin,
+      linkGithub: 'https://github.com/cupyusuf/',
+      linkLinkedin: 'https://www.linkedin.com/in/yusuf-supriadi/',
     },
   ]
 
@@ -58,7 +75,7 @@ function AboutPage() {
       <Navbar />
       <div className='pt-10'>
         <div className='flex flex-col items-center '>
-          <div className='bg-orange-500 justify-center rounded-t rounded-2xl max-w-[50px] px-20 pb-10 md:max-w-[850px] pt-10'>
+          <div className='bg-orange-500 justify-center rounded-t rounded-2xl max-w-[500px] px-20 pb-10 md:max-w-[850px] pt-10'>
             <div className='items-center'>
               <img src={imgAbout} alt='img-about' className='w-200'></img>
               <div>
@@ -78,13 +95,13 @@ function AboutPage() {
           {listTeam.map((team) => (
             <div className='border border-black rounded-xl p-5 text-center shadow-md'>
               <div className='flex flex-row justify-center mt-2'>
-                <img alt='avatar' className='w-32' src={team.image}></img>
+                <img alt='avatar' className='w-32 h-32 border rounded-full bg-center' src={team.image}></img>
               </div>
               <p className='font-bold text-md'>{team.name}</p>
               <p className='text-gray-600 text-xs'>{team.role}</p>
               <div className='flex flex-row justify-center mt-2'>
-                <a href="/" ><img src={team.github} alt='github-logo' className='w-8'/></a>
-                <a href="/" ><img src={team.linkedin} alt='linkedin-logo' className='w-8' /></a>
+                <a href={team.linkGithub} ><img src={team.github} alt='github-logo' className='w-8'/></a>
+                <a href={team.linkLinkedin} ><img src={team.linkedin} alt='linkedin-logo' className='w-8' /></a>
               </div>
             </div>
           ))}
